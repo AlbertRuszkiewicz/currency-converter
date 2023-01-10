f9d37017111cb0cc2d6d89176cd1005174fc82d3
Albert Ruszkiewicz <ruszkiewicz.albert@gmail.com>
2023-01-10 21:00:31 +0100

    Refactoring script to use functions

--- js/script.js
+++ js/script.js
@@ -1,51 +1,49 @@
-console.log("Hello Dev");
+{
+    const currencyAmountCalc = (plnAmountElement, exchangeElement) => {
+        const exchange = +exchangeElement.value.slice(4);
+        return (plnAmountElement.value / exchange).toFixed(2);
+    };
+    const plnAmountCalc = (currencyAmountElement, exchangeElement) => {
+        const exchange = +exchangeElement.value.slice(4);
+        return (currencyAmountElement.value * exchange).toFixed(2);
+    };
+    const exchangeModification = (exchangeElement) => {
+        let currencyFullNameElement = document.querySelector(".js-currencyFullName");
+        let currencyShortNameElement = document.querySelector(".js-currencyShortName");
+        const currencyName = exchangeElement.value.slice(0, 3);
+        switch (currencyName) {
+            case "USD":
+                currencyShortNameElement.innerText = currencyName;
+                currencyFullNameElement.innerText = "Dolara amerykańskiego";
+                break;
+            case "UAH":
+                currencyShortNameElement.innerText = currencyName;
+                currencyFullNameElement.innerText = "Hrywny";
+                break;
+            case "GBP":
+                currencyShortNameElement.innerText = currencyName;
+                currencyFullNameElement.innerText = "Funta brytyjskiego";
+                break;
+            case "CHF":
+                currencyShortNameElement.innerText = currencyName;
+                currencyFullNameElement.innerText = "Franka Szwajcarskiego";
+                break;
+            default:
+                currencyShortNameElement.innerText = currencyName;
+                currencyFullNameElement.innerText = "Euro";
+                break;
+        };
+    };
+    const init = () => {
+        console.log("Hello Dev");
 
-let currencyAmountElement = document.querySelector(".js-currencyAmount");
-let exchangeElement = document.querySelector(".js-exchange");
-let plnAmountElement = document.querySelector(".js-plnAmount");
-let currencyFullNameElement = document.querySelector(".js-currencyFullName");
-let currencyShortNameElement = document.querySelector(".js-currencyShortName");
+        let currencyAmountElement = document.querySelector(".js-currencyAmount");
+        const exchangeElement = document.querySelector(".js-exchange");
+        let plnAmountElement = document.querySelector(".js-plnAmount");
 
-
-
-exchangeElement.addEventListener("input", () => {
-    let plnAmount = plnAmountElement.value;
-    let exchange = +exchangeElement.value.slice(4);
-    currencyAmountElement.value = (plnAmount / exchange).toFixed(2);
-
-    let currencyName = exchangeElement.value.slice(0, 3);
-    switch (currencyName) {
-        case "USD":
-            currencyShortNameElement.innerText = currencyName;
-            currencyFullNameElement.innerText = "Dolara amerykańskiego";
-            break;
-        case "UAH":
-            currencyShortNameElement.innerText = currencyName;
-            currencyFullNameElement.innerText = "Hrywny";
-            break;
-        case "GBP":
-            currencyShortNameElement.innerText = currencyName;
-            currencyFullNameElement.innerText = "Funta brytyjskiego";
-            break;
-        case "CHF":
-            currencyShortNameElement.innerText = currencyName;
-            currencyFullNameElement.innerText = "Franka Szwajcarskiego";
-            break;
-        default:
-            currencyShortNameElement.innerText = currencyName;
-            currencyFullNameElement.innerText = "Euro";
-            break;
-    }
-});
-
-currencyAmountElement.addEventListener("input", () => {
-    let currencyAmount = currencyAmountElement.value;
-    let exchange = +exchangeElement.value.slice(4);
-    plnAmountElement.value = (currencyAmount * exchange).toFixed(2);
-});
-
-plnAmountElement.addEventListener("input", () => {
-    let plnAmount = plnAmountElement.value;
-    let exchange = +exchangeElement.value.slice(4);
-    currencyAmountElement.value = (plnAmount / exchange).toFixed(2);
-});
+        exchangeElement.addEventListener("input", () => { exchangeModification(exchangeElement); currencyAmountElement.value = currencyAmountCalc(plnAmountElement, exchangeElement); });
+        plnAmountElement.addEventListener("input", () => { currencyAmountElement.value = currencyAmountCalc(plnAmountElement, exchangeElement) });
+        currencyAmountElement.addEventListener("input", () => { plnAmountElement.value = plnAmountCalc(currencyAmountElement, exchangeElement) });
+    };
+    init();
+}
